bccbfa3a4fae666daa13912bf4cb645e298ff293
Asnise_Git <55057145+asnise@users.noreply.github.com>
2021-06-16 21:48:04 +0700

    Add files via upload

--- Script.js
+++ Script.js
@@ -1,9 +1,14 @@
+var log;
+var data_load;
+var data_load_body = "";
 $(document).ready(function(){
 $.ajax({
   url: 'https://api.github.com/repos/asnise/NPC-Ann-SDV/releases/latest' ,
   format: 'json',
   dataType: "json",
   success(data){
+	  data_load = data;
+	  data_load_body = data_load.body.toString();
      let version = $('<h4>').text("An In Stardew Version " + data.tag_name);
      $('#ver')
      .append(version);
@@ -22,7 +27,12 @@ function cansel_win(idtag ,name,content)
 	{
 		win_alert.style.display = "block";
 	}
-			document.getElementById("title_name").innerHTML = name;
+	if(content = "log")
+	{
+		content = data_load_body.slice(0,75).replace("\n-","<br>-").replace("\n-","<br>-");
+		console.log(content);
+	}
+		document.getElementById("title_name").innerHTML = name;
 		document.getElementById("content_mod").innerHTML = content;
 }
 
